ff4d28236a6381cea2fd177ab0020a66acef5ff0
drkenni <tanyromachka1@mail.ru>
2023-07-11 12:58:03 +0300

    4 HW done

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -6,10 +6,11 @@ ALTER TABLE products ADD CONSTRAINT chk_products_unit_price CHECK (unit_price > 
 ALTER TABLE products ADD CONSTRAINT chk_products_discontinued CHECK (discontinued IN (0,1))
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
-SELECT * INTO dis_roducts FROM products WHERE discontinued = 1
+SELECT * INTO dis_products FROM products WHERE discontinued = 1
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
-ALTER TABLE products DISABLE TRIGGER fk_products_;
+ALTER TABLE order_details DROP CONSTRAINT fk_order_details_products;
+ALTER TABLE order_details ADD CONSTRAINT fk_order_details_products FOREIGN KEY (product_id)
+REFERENCES products(product_id) ON DELETE CASCADE;
 DELETE FROM products WHERE discontinued = 1;
-ALTER TABLE products ENABLE TRIGGER ALL;
